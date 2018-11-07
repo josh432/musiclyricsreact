ecd0488ec1e851aa69c4d93eb0acaf542b49f66a
Josh Silverman <jrsilverman71@hotmail.com>
2018-11-07 12:04:19 -0800

    prettier

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import Tracks from '../tracks/Tracks';
+import React from "react";
+import Tracks from "../tracks/Tracks";
 
 const Index = () => {
   return (
     <React.Fragment>
-        <Tracks />
-
+      <Tracks />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Index;
